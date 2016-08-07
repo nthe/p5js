1d32310411e0b9d68ea8cc3186014f835a181e36
Juraj <Juraj@JO.local>
2016-08-07 14:52:58 +0200

    updated 0002

--- 0002/sketch.js
+++ 0002/sketch.js
@@ -1,6 +1,5 @@
 
 var a = 0;
-var nodes = []
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
@@ -12,14 +11,12 @@ function setup(){
 function draw(){
   background(250);
   noFill();
-  nodes = []
   for(var y = 0; y < 20; y++){
     a += 0.3165;
     for(var x = 0; x < 20; x++){
       push();
-      nodes.push({"pos": [(x * 30) + width/2 - (300), (y * 30) + height/2 - (300)], "angle": sin(a % TWO_PI)});
-      translate(nodes[nodes.length -1 ].pos[0], nodes[nodes.length-1].pos[1]);
-      rotate(nodes[nodes.length-1].angle);
+      translate((x * 30) + width/2 - 300, (y * 30) + height/2 - 300);
+      rotate(sin(a % TWO_PI));
       ellipse(0, 0, 20, 11);
       ellipse(0, 0, 20, 3);
       ellipse(0, 0, 19, 5);
