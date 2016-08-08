24c5d4ec95fcad986a6f911ce5a8dedf99f5d884
Juraj <Juraj@JO.local>
2016-08-08 18:47:58 +0200

    updated 0003

--- 0003/sketch.js
+++ 0003/sketch.js
@@ -1,57 +1,32 @@
 
-
-var n = null;
-var mouse_pos = null;
-var easing = .05;
-var max_velocity = 5.;
-var init = false;
+var a = 0; r = 0;
 
 function setup(){
-  noFill();
-  mouse_pos = createVector(touchX, touchY);
   createCanvas(windowWidth, windowHeight);
-  stroke(50);
-  // frameRate(50);
-  n = new Node(createVector(windowWidth / 2, windowHeight / 2));
+  noFill();
+  stroke(50, 150);
+  r = windowWidth / 6;
+  strokeWeight(0.3);
   background(250);
 }
 
 function draw(){
-  if(init){
-    background(250);
-    n.update();
-    if(n.pos.dist(mouse_pos) < .5){
-      noLoop();
-    }
-  } else {
-
-      ellipse(windowWidth / 2, windowHeight / 2, 10, 10);
-    // noLoop();
-  }
-}
-
-function mouseMoved(){
-  // if(!init) init = !init;
-  init = true;
-  redraw();
-}
-
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
   background(250);
-}
+  push();
+  a += .005;
+  translate(windowWidth/2, windowHeight/2);
+  rotate(a);
+  beginShape(TRIANGLE_STRIP);
+  for(var phi = 0; phi <= TWO_PI; phi += 0.05){
+  for(var theta = 0; theta <= TWO_PI; theta += 0.5){
+      curveVertex(
+        (r) * cos(theta) * sin(phi + a),
+        (r) * sin(theta) * sin(phi),
+        (r + a) * cos(phi)
+      );
+    }
 
-
-function Node(_v){
-  this.pos = _v;
-  this.vel = createVector(0, 0);
-
-  this.update = function(){
-    mouse_pos.x = touchX;
-    mouse_pos.y = touchY;
-    this.vel = p5.Vector.sub(mouse_pos, this.pos).mult(easing);
-    this.vel.limit(max_velocity);
-    this.pos.add(this.vel);
-    ellipse(this.pos.x, this.pos.y, 10, 10);
   }
+  endShape();
+  pop();
 }
